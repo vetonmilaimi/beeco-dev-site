1ebe6bfebed72a24f6e9fec343bd18fb7b453bdc
vetonmilaimi <vetonmila@gmail.com>
2023-12-11 01:43:52 +0100

    UI card ability to change the side of image

--- src/ui/card.tsx
+++ src/ui/card.tsx
@@ -2,11 +2,16 @@ interface Props {
 	image: ImageMetadata
 	title: string
 	description: string
+	imgRight?: boolean
 }
 
-const Card = ({ image, title, description }: Props) => {
+const Card = ({ image, title, description, imgRight }: Props) => {
 	return (
-		<div className="max-w-[70%] mx-auto bg-red-400 my-3 px-12 flex py-6 items-center justify-between">
+		<div
+			className={`max-w-[70%] mx-auto bg-red-400 my-3 px-12 flex ${
+				imgRight && 'flex-row-reverse'
+			} py-6 items-center justify-between`}
+		>
 			<img src={image.src} width={300} />
 			<div className="flex flex-col p-6">
 				<h1 className="text-[#002A41] text-4xl pb-10">{title}</h1>
